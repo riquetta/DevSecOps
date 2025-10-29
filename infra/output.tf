73570d44586d1f064d6a7211ddca859bf2ead8bf
riquetta <37386067+riquetta@users.noreply.github.com>
2025-10-29 11:28:31 -0600

    Add Terraform infra + Kubernetes manifests for AKS deployment

--- infra/output.tf
+++ infra/output.tf
@@ -11,3 +11,8 @@ value = azurerm_storage_account.st.id
 output "aks_cluster_name" {
 value = azurerm_kubernetes_cluster.aks.name
 }
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
